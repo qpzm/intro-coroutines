b512dadc1e87399c99361fc15bbfc561fd4f6039
Hyunmin Lee <hyunmin@toss.im>
2020-11-02 03:52:11 +0900

    4-1. Try Suspend mode in swing
    concurrency가 없어서 시간은 그대로 24초

--- src/tasks/Request4Suspend.kt
+++ src/tasks/Request4Suspend.kt
@@ -1,7 +1,20 @@
 package tasks
 
 import contributors.*
+import retrofit2.Response
 
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgReposCall(req.org)
+        .execute() // Executes request and blocks the current thread
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    return repos.flatMap { repo ->
+        service
+            .getRepoContributorsCall(req.org, repo.name)
+            .execute() // Executes request and blocks the current thread
+            .also { logUsers(repo, it) }
+            .bodyList()
+    }.aggregate()
 }
