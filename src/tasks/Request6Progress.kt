b1fbe023de5be1f34f86432aa0ad4c33995e9425
qpzm <lhm1442@gmail.com>
2020-11-03 04:01:46 +0900

    6-1. Run suspend fun one by one

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -1,11 +1,52 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    var allUsers = emptyList<User>()
+    for ((index, repo) in repos.withIndex()) {
+        val users = service.getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        // One-liner, but the same
+        // allUsers = (allUsers + users).aggregate()
+        allUsers += users
+        allUsers = allUsers.aggregate()
+        updateResults(allUsers, index == repos.lastIndex)
+    }
 }
+
+/*
+suspend fun loadContributorsProgress(
+    service: GitHubService,
+    req: RequestData,
+    updateResults: suspend (List<User>, completed: Boolean) -> Unit
+) {
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    var allUsers = emptyList<User>()
+    for ((index, repo) in repos.withIndex()) {
+        val users = service.getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+
+        allUsers = (allUsers + users).aggregate()
+        updateResults(allUsers, index == repos.lastIndex)
+    }
+}
+ */
