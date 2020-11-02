010a2aee6e11cf8663d35a05cf94af67648cf885
Hyunmin Lee <hyunmin@toss.im>
2020-11-02 22:59:50 +0900

    5-1. Concurrency

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,21 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgReposCall(req.org)
+        .execute() // Executes request and blocks the current thread
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val deferred = repos.map { repo ->
+        async {
+            service
+                .getRepoContributorsCall(req.org, repo.name)
+                .execute() // Executes request and blocks the current thread
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+
+    deferred.awaitAll().flatten().aggregate()
 }
