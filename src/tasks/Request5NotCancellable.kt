29131051d2d786dceb1126017c4b8e6f241c8126
Hyunmin Lee <hyunmin@toss.im>
2020-11-02 23:38:16 +0900

    5-2. GlobalScope.async is not cancellable

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -5,5 +5,21 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgReposCall(req.org)
+        .execute() // Executes request and blocks the current thread
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val deferred = repos.map { repo ->
+        GlobalScope.async {
+            service
+                .getRepoContributorsCall(req.org, repo.name)
+                .execute() // Executes request and blocks the current thread
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+
+    return deferred.awaitAll().flatten().aggregate()
 }
