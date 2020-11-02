b3e30a26a5599954999198120fb755a3a10be653
qpzm <lhm1442@gmail.com>
2020-11-03 04:27:29 +0900

    7-1 Use channel to run suspend fun concurrently

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -10,7 +10,26 @@ suspend fun loadContributorsChannels(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
     coroutineScope {
-        TODO()
+        val channel = Channel<List<User>>()
+        for (repo in repos) {
+            launch {
+                val users = service.getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(users)
+            }
+        }
+        var allUsers = emptyList<User>()
+        repeat(repos.size) {
+            val users = channel.receive()
+            allUsers = (allUsers + users).aggregate()
+            updateResults(allUsers, it == repos.lastIndex)
+        }
     }
 }
